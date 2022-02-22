20ca7a2fa5101c632676635e37c14c267b4dfda4
heitorgonc <heitor@hypersoft.com.br>
2022-02-22 14:34:26 -0300

    writing a txt file through an API

---
+++ ex1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	</head>
+	<body>
+		<div>
+			<form method="POST" id="frmUsuario">
+				<div>
+					<input type="text" name="usuario" id="usuario">
+					<input type="submit" name="saveUsuario" id="saveUsuario" class="submit" value="Salvar">
+				</div>			
+			</form>
+		</div>
+		<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="crossorigin="anonymous"></script>
+		<script>
+			$('.submit').on('click', event => {
+				event.preventDefault();
+				const data = {
+					usuario: $('#usuario').val(),
+					action: $(event.target).prop('name'),
+				};
+				$.ajax('/ex1api.php', {
+					method: 'post',
+					data: JSON.stringify(data),
+					success: resp => {
+						console.log(resp);
+					}
+				});
+			});
+		</script>
+	</body>
+</html>
---
+++ ex1api.php
@@ -0,0 +1,15 @@
+<?php
+	$json = file_get_contents('php://input');
+	$obj = json_decode($json);
+	
+	if($obj->action == 'saveUsuario'){
+		$handle = fopen('users.txt', 'a+');
+		fwrite($handle, $obj->usuario . "\n");
+		fclose($handle);
+
+		header('Content-Type: application/json; charset=utf-8', true, 202);
+		echo json_encode(
+			['status' => 'ok', 'message' => 'usuário ' . $obj->email . ' gravado com sucesso']
+		);
+	}
+?>
